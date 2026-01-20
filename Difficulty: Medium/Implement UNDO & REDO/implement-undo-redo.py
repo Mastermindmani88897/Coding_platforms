8ec2b2fe3368ad3d13f392e009bb93eee3773877
Mastermindmani88897 <147472580+Mastermindmani88897@users.noreply.github.com>
2026-01-20 13:54:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement UNDO & REDO/implement-undo-redo.py
@@ -0,0 +1,25 @@
+class Solution:
+    def __init__(self):
+        self.doc = []
+        self.undo_stack = []
+        self.redo_stack = []
+
+    def append(self, x):
+        self.doc.append(x)
+        self.undo_stack.append(x)
+        self.redo_stack.clear()
+
+    def undo(self):
+        if self.undo_stack:
+            ch = self.undo_stack.pop()
+            self.doc.pop()
+            self.redo_stack.append(ch)
+
+    def redo(self):
+        if self.redo_stack:
+            ch = self.redo_stack.pop()
+            self.doc.append(ch)
+            self.undo_stack.append(ch)
+
+    def read(self):
+        return ''.join(self.doc)
