67f9b0cca912aca4eab83e978dcfdfa4f5f6aed9
Mastermindmani88897 <147472580+Mastermindmani88897@users.noreply.github.com>
2026-02-04 18:48:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Last Moment Before All Ants Fall Out/last-moment-before-all-ants-fall-out.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getLastMoment(self, n, left, right):
+        ans = 0
+        for x in left:
+            ans = max(ans, x)
+        for x in right:
+            ans = max(ans, n - x)
+        return ans
