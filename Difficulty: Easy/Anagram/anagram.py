3f4f615683bf5c2bf65ccff05ee83e3468f5bbbd
Mastermindmani88897 <147472580+Mastermindmani88897@users.noreply.github.com>
2026-02-26 19:26:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def areAnagrams(self, s1, s2):
+        if len(s1) != len(s2):
+            return False
+        
+        count = [0] * 26
+        
+        for i in range(len(s1)):
+            count[ord(s1[i]) - ord('a')] += 1
+            count[ord(s2[i]) - ord('a')] -= 1
+        
+        for c in count:
+            if c != 0:
+                return False
+        
+        return True
