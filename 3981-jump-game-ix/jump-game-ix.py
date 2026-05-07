ef44aaa5a5a99ebae4b724a779315d84228abcbd
Mastermindmani88897 <147472580+Mastermindmani88897@users.noreply.github.com>
2026-05-07 19:01:58 +0530

    Time: 296 ms (26.72%) | Memory: 39.6 MB (62.07%) - LeetSync

---
+++ 3981-jump-game-ix/jump-game-ix.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maxValue(self, nums):
+        n = len(nums)
+        
+        pref = [0] * n
+        suff = [0] * n
+        
+        pref[0] = nums[0]
+        for i in range(1, n):
+            pref[i] = max(pref[i - 1], nums[i])
+        
+        suff[n - 1] = nums[n - 1]
+        for i in range(n - 2, -1, -1):
+            suff[i] = min(suff[i + 1], nums[i])
+        
+        ans = [0] * n
+        
+        start = 0
+        
+        for i in range(n - 1):
+            if pref[i] <= suff[i + 1]:
+                mx = max(nums[start:i + 1])
+                
+                for j in range(start, i + 1):
+                    ans[j] = mx
+                
+                start = i + 1
+        
+        mx = max(nums[start:])
+        
+        for j in range(start, n):
+            ans[j] = mx
+        
+        return ans
