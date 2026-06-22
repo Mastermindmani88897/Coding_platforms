18fac1c9209a492d37d66d31bce146ea6aaf5a15
Mastermindmani88897 <147472580+Mastermindmani88897@users.noreply.github.com>
2026-06-22 19:39:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Area Between Bars/maximum-area-between-bars.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height):
+        n = len(height)
+        
+        if n < 2:
+            return 0
+        
+        i, j = 0, n - 1
+        ans = 0
+        
+        while i < j:
+            width = j - i - 1
+            ans = max(ans, min(height[i], height[j]) * width)
+            
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        
+        return ans
